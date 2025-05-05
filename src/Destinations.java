9acb1b141bd40ff4555930e2acf98ba362db995e
Joshua Carreon <j.carreon@lancaster.ac.uk>
2025-05-05 15:54:12 +0100

    making task1 skeleton code

---
+++ src/Destinations.java
@@ -0,0 +1,15 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class GetStations
+{
+    private String FilePath;
+
+    public void findStations()
+    {
+
+    }
+}
