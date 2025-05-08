99cb644a04d410bc62cb5510ba4b08db0f665e88
Joshua Carreon <j.carreon@lancaster.ac.uk>
2025-05-08 02:37:42 +0100

    started a Dijkstra class, processes the algorithm

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -1,10 +1,55 @@
-// Java Program to Implement Dijkstra's Algorithm
-// Using Priority Queue
+import org.w3c.dom.Node;
 
-// Importing utility classes
 import java.util.*;
-
-// Main class DPQ
+// Dijkstra priority queue
 public class Dijkstra {
+    private int ShortDistance[];
+    private Set<Integer> settledNodes;
+    private PriorityQueue<Node> pq;
+    private int Verticies;
+    List<List<Node>> AdjacencyList;
 
+    public Dijkstra(int Verticies) {
+        this.Verticies = Verticies;
+        ShortDistance = new int[Verticies];
+        settledNodes = new HashSet<Integer>();
+        pq = new PriorityQueue<Node>(Verticies, new Node());
+    }
+
+    public void dijkstra() {
+
+    }
+
+    private void adjacentNodes() {
+
+    }
+
+    //for the custom sorting order
+    //orders nodes for sorting
+    //what is bigger is put further front of queue else equal
+    class Node implements Comparator<Node> {
+        public int node;
+        public int cost;
+
+        public Node() {
+        }
+
+        public Node(int node, int cost) {
+            this.node = node;
+            this.cost = cost;
+        }
+
+     //or
+        public int compare(Node node1, Node node2) {
+            if (node1.cost < node2.cost)
+                return -1;
+            if (node1.cost > node2.cost)
+                return 1;
+
+            return 0;
+        }
+
+    }
 }
+
+
